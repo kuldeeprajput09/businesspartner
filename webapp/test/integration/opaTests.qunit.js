7ef9f5ddb5d73577c38a47e90329987ae94a0279
Kuldeep Singh <kuldeepthakur041@gmail.com>
2022-06-08 08:46:51 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["businesspartner/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"businesspartner/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
